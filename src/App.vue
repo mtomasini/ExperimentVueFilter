0af29967fb2498fabf79bd682f8bd36018b03fc0
Matt Tomasini <matteo.tomasini@protonmail.com>
2022-12-15 16:30:20 +0100

    Worked hard on search bar, still no luck in making it responsive

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import SearchBar from './components/SearchBar.vue';
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
@@ -8,40 +9,9 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <h1 class="green">HELLO! Here's a list: </h1>
+      <SearchBar />
     </div>
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,33 @@
+<script setup>
+import platforms from "@/assets/platforms.json";
+
+let toPrint = tagsToFilter
+
+// export default {
+//     setup() {
+//         let tagsToFilter = ref("")
+//         //let listOfPlatforms = platforms
+//         const filteredList = computed(() => {
+//             let filteredTags = tagsToFilter.value
+//             if (!filter.length) return platforms
+//             return platforms.filter((item) => item.tags.includes(filteredTags))
+//         })
+//         return {
+//             tagsToFilter,
+//             filteredList,
+//         }
+//     }
+// }
+
+</script>
+
+<template>
+    <div class="search-bar">
+            <input class="search-box" v-model="tagsToFilter" placeholder="Filter by tag..."/>
+    </div>
+    <ul>
+        <li v-for="item in platforms" class="blue">
+            {{ item.title }}, {{ toPrint }}
+        </li>
+    </ul>
+</template>
